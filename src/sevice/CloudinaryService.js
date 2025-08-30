4ef47c4e683d35366615824f221ddc8e33c53b75
shivamsaw-304 <shivamsaw304@gmail.com>
2025-08-30 11:30:30 +0530

    Merge remote-tracking branch 'origin/main'

--- src/sevice/CloudinaryService.js
+++ src/sevice/CloudinaryService.js
@@ -1,14 +1,15 @@
 import axios from "axios";
-//import {image} from "html2canvas/dist/types/css/types/image.js";
 
-export const uploadInvoiceThumbail = async (imageData) => {
+export const uploadInvoiceThumbnail = async (imageData) => {
     const formData = new FormData();
-    formData.append('file',imageData);
-    formData.append('upload_present','invoices-thumbnail');
-    formData.append('cloud_name','dwt7au6z4')
+    formData.append('file', imageData);
+    formData.append('upload_preset', 'invoices-thumbnail'); // ✅ correct spelling
+    // formData.append('cloud_name','dwt7au6z4'); // ❌ not needed
 
-    const response =  await axios.post(`https://api.cloudinary.com/v1_1/dwt7au6z4/image/upload`,formData,);
+    const response = await axios.post(
+        `https://api.cloudinary.com/v1_1/dwt7au6z4/image/upload`,
+        formData
+    );
 
     return response.data.secure_url;
-
 }
